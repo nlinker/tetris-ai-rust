092fcdc744bc132905553a8352f551d0e2e7b6de
Nick Linker <nlinker@gmail.com>
2019-07-05 22:29:44 +0600

    introduce Piece type

--- src/model.rs
+++ src/model.rs
@@ -11,6 +11,12 @@ pub struct RawPiece<'a> {
 #[derive(Debug, Eq, PartialEq)]
 pub struct Point(pub i32, pub i32);
 
+#[derive(Debug, Eq, PartialEq)]
+pub struct Piece {
+    pub diffs: Vec<Point>,
+    pub shift: Point,
+}
+
 pub const I: RawPiece<'static> = RawPiece {
     field: r#"
         . . . .
@@ -84,21 +90,21 @@ pub const Z: RawPiece<'static>  = RawPiece {
 
 
 /// return the piece points relative of (0, 0) with parity
-pub fn convert(src: RawPiece<'_>) -> (Vec<Point>, Point) {
-    let mut vec: Vec<Point> = Vec::with_capacity(4);
+pub fn convert(src: RawPiece<'_>) -> Piece {
+    let mut diffs: Vec<Point> = Vec::with_capacity(4);
     let mut ci = 0;
-    // parity is needed to know how to round the piece after the rotation
-    let mut parity_i = 0;
-    let mut parity_j = 0;
+    // shift is needed to know how to round the piece after the rotation
+    let mut shift_i = 0;
+    let mut shift_j = 0;
     for line in src.field.trim_indent().split('\n') {
         let mut cj = 0;
         for c in line.chars() {
             if c == '*' {
                 let i = (2.0 * ((ci as f32) - src.ri)).trunc() as i32;
                 let j = (2.0 * ((cj as f32) - src.rj)).trunc() as i32;
-                vec.push(Point(i, j));
-                if i % 2 == 1 { parity_i = 1; }
-                if j % 2 == 1 { parity_j = 1; }
+                diffs.push(Point(i, j));
+                if i % 2 == 1 { shift_i = 1; }
+                if j % 2 == 1 { shift_j = 1; }
                 cj += 1;
             } else if c == '.' {
                 cj += 1;
@@ -106,7 +112,11 @@ pub fn convert(src: RawPiece<'_>) -> (Vec<Point>, Point) {
         }
         ci += 1;
     }
-    (vec, Point(parity_i, parity_j))
+    Piece { diffs, shift: Point(shift_i, shift_j) }
+}
+
+fn try_position(field: Vec<Vec<u8>>, base: Point, piece: Piece) -> Option<Vec<Point>> {
+    None
 }
 
 
--- tests/model.rs
+++ tests/model.rs
@@ -1,28 +1,35 @@
 #![feature(type_ascription)]
 
 //use std::collections::HashSet;
-use tetris::model::{Point, convert, I, O, L, J, T, S, Z};
+use tetris::model::{Point, convert, I, O, L, J, T, S, Z, Piece};
 
 #[test]
 fn test_conversion() {
-    let xs = vec![Point(0, -3), Point(0, -1), Point(0, 1), Point(0, 3)]: Vec<Point>;
-    assert_eq!((xs, Point(0, 1)), convert(I));
+    let diffs = vec![Point(0, -3), Point(0, -1), Point(0, 1), Point(0, 3)]: Vec<Point>;
+    let shift = Point(0, 1);
+    assert_eq!(Piece { diffs, shift }, convert(I));
 
-    let xs = vec![Point(-1, -1), Point(-1, 1), Point(1, -1), Point(1, 1)]: Vec<Point>;
-    assert_eq!((xs, Point(1, 1)), convert(O));
+    let diffs = vec![Point(-1, -1), Point(-1, 1), Point(1, -1), Point(1, 1)]: Vec<Point>;
+    let shift = Point(1, 1);
+    assert_eq!(Piece { diffs, shift }, convert(O));
 
-    let xs = vec![Point(-2, -1), Point(0, -1), Point(2, -1), Point(2, 1)]: Vec<Point>;
-    assert_eq!((xs, Point(0, 1)), convert(L));
+    let diffs = vec![Point(-2, -1), Point(0, -1), Point(2, -1), Point(2, 1)]: Vec<Point>;
+    let shift = Point(0, 1);
+    assert_eq!(Piece { diffs, shift }, convert(L));
 
-    let xs = vec![Point(-2, 1), Point(0, 1), Point(2, -1), Point(2, 1)]: Vec<Point>;
-    assert_eq!((xs, Point(0, 1)), convert(J));
+    let diffs = vec![Point(-2, 1), Point(0, 1), Point(2, -1), Point(2, 1)]: Vec<Point>;
+    let shift = Point(0, 1);
+    assert_eq!(Piece { diffs, shift }, convert(J));
 
-    let xs = vec![Point(-1, -2), Point(-1, 0), Point(-1, 2), Point(1, 0)]: Vec<Point>;
-    assert_eq!((xs, Point(1, 0)), convert(T));
+    let diffs = vec![Point(-1, -2), Point(-1, 0), Point(-1, 2), Point(1, 0)]: Vec<Point>;
+    let shift = Point(1, 0);
+    assert_eq!(Piece { diffs, shift }, convert(T));
 
-    let xs = vec![Point(-1, 0), Point(-1, 2), Point(1, -2), Point(1, 0)]: Vec<Point>;
-    assert_eq!((xs, Point(1, 0)), convert(S));
+    let diffs = vec![Point(-1, 0), Point(-1, 2), Point(1, -2), Point(1, 0)]: Vec<Point>;
+    let shift = Point(1, 0);
+    assert_eq!(Piece { diffs, shift }, convert(S));
 
-    let xs = vec![Point(-1, -2), Point(-1, 0), Point(1, 0), Point(1, 2)]: Vec<Point>;
-    assert_eq!((xs, Point(1, 0)), convert(Z));
+    let diffs = vec![Point(-1, -2), Point(-1, 0), Point(1, 0), Point(1, 2)]: Vec<Point>;
+    let shift = Point(1, 0);
+    assert_eq!(Piece { diffs, shift }, convert(Z));
 }
