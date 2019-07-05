7afbff4bad3a17660d77c20d9d62c82edfb22c00
Nick Linker <nlinker@gmail.com>
2019-07-06 03:11:47 +0600

    fix tests

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,21 @@
 use tetris::model::{I, T, build_piece};
 use tetris::utils::Trim;
 
-fn build_piece(p_str: &str) -> Vec<u8> {
-    let mut cells: Vec<u8> = vec![0; 16];
-    // let mut cells: Vec<Vec<u8>> = vec![vec![0; n]; m];
-    let mut i = 0;
-    for line in p_str.trim_indent().split('\n') {
-        let line: &str = line;
-        let chars = line.chars().filter(|c| !c.is_whitespace()).collect::<Vec<_>>();
-        cells[i * 4 + 0] = if chars[0] == '.' { 0 } else { 1 };
-        cells[i * 4 + 1] = if chars[1] == '.' { 0 } else { 1 };
-        cells[i * 4 + 2] = if chars[2] == '.' { 0 } else { 1 };
-        cells[i * 4 + 3] = if chars[3] == '.' { 0 } else { 1 };
-        i += 1;
-    }
-    cells
-}
+//fn build_piece(p_str: &str) -> Vec<u8> {
+//    let mut cells: Vec<u8> = vec![0; 16];
+//    // let mut cells: Vec<Vec<u8>> = vec![vec![0; n]; m];
+//    let mut i = 0;
+//    for line in p_str.trim_indent().split('\n') {
+//        let line: &str = line;
+//        let chars = line.chars().filter(|c| !c.is_whitespace()).collect::<Vec<_>>();
+//        cells[i * 4 + 0] = if chars[0] == '.' { 0 } else { 1 };
+//        cells[i * 4 + 1] = if chars[1] == '.' { 0 } else { 1 };
+//        cells[i * 4 + 2] = if chars[2] == '.' { 0 } else { 1 };
+//        cells[i * 4 + 3] = if chars[3] == '.' { 0 } else { 1 };
+//        i += 1;
+//    }
+//    cells
+//}
 
 
 
--- tests/model.rs
+++ tests/model.rs
@@ -79,7 +79,22 @@ fn test_try_position() {
         width: 4,
     };
     let piece_i = build_piece(I);
-    assert_eq!(None, try_position(&field, &Point(1, 1), &piece_i, 0));
+
+    let expected = Some(vec![Point(1, 0), Point(1, 1), Point(1, 2), Point(1, 3)]);
+    assert_eq!(expected, try_position(&field, &Point(1, 2), &piece_i, 0));
+
+    // note the same as above, but just the order is different
+    let expected = Some(vec![Point(1, 3), Point(1, 2), Point(1, 1), Point(1, 0)]);
+    assert_eq!(expected, try_position(&field, &Point(1, 2), &piece_i, 2));
+
+    let expected = Some(vec![Point(3, 2), Point(2, 2), Point(1, 2), Point(0, 2)]);
+    assert_eq!(expected, try_position(&field, &Point(2, 2), &piece_i, 1));
+
+    let expected = Some(vec![Point(0, 2), Point(1, 2), Point(2, 2), Point(3, 2)]);
+    assert_eq!(expected, try_position(&field, &Point(2, 2), &piece_i, 3));
+
+    let expected = None;
+    assert_eq!(expected, try_position(&field, &Point(3, 2), &piece_i, 1));
 }
 
 pub fn convert_field(_: &str) -> Field {
