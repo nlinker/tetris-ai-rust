f2d0ffb1807f87e64522749254b709a92f081b02
Nick Linker <nlinker@gmail.com>
2019-07-07 12:39:26 +0600

    fix a little

--- src/main.rs
+++ src/main.rs
@@ -7,18 +7,8 @@ extern crate lazy_static;
 //const TEST: (String, Point) = ("xxx".into(), Point(0, 0));
 use tetris::model::{SHAPES, Shape, initial_state};
 
-//fn main() {
-//    //for shape in &*SHAPES {
-//    //    println!("Shapes: {:?}", shape);
-//    //}
-//
-//    let gs = initial_state(10, 20, Some(69));
-//    println!("{:?}", gs);
-//}
-
-
 fn main() {
-    let mut gs = initial_state(20, 10, Some(68));
+    let mut gs = initial_state(15, 10, Some(68));
     // curr_shape_idx = 3
     println!("{}", gs);
 }
